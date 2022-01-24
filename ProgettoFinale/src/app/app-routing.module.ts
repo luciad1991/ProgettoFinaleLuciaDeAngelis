93a498656aa28b3011cde225f304475b46d95684
luciad1991 <lucyale91@gmail.com>
2022-01-24 21:03:24 +0100

    commit

--- ProgettoFinale/src/app/app-routing.module.ts
+++ ProgettoFinale/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddClientComponent } from './add-client/add-client.component';
 import { ClientDetailsComponent } from './client-details/client-details.component';
 import { ClientsTableComponent } from './clients-table/clients-table.component';
 
 const routes: Routes = [
   {path: 'Clients', component: ClientsTableComponent},
-  {path: 'Client detail' ,component: ClientDetailsComponent}
+  {path: 'ClientDetail' ,component: ClientDetailsComponent},
+  {path: 'AddClient', component: AddClientComponent}
 ];
 
 @NgModule({
